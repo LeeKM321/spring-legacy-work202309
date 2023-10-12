3ab479175395ff3a443da9e809b0167205fe5229
kyoungmin <stephen4951@gmail.com>
2023-10-12 13:33:51 +0900

    20231012 MyWeb 테스트 완료 및 템플릿 적용

--- MyWeb/src/main/java/com/spring/myweb/freeboard/controller/FreeBoardController.java
+++ MyWeb/src/main/java/com/spring/myweb/freeboard/controller/FreeBoardController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.spring.myweb.freeboard.dto.FreeModifyRequestDTO;
 import com.spring.myweb.freeboard.dto.FreeRegistRequestDTO;
 import com.spring.myweb.freeboard.service.IFreeBoardService;
 
@@ -26,6 +27,9 @@ public class FreeBoardController {
 		model.addAttribute("boardList", service.getList());
 	}
 	
+	//글쓰기 페이지를 열어주는 메서드
+	@GetMapping("/freeRegist")
+	public void regist() {}
 	
 	//글 등록 처리
 	@PostMapping("/freeRegist")
@@ -33,6 +37,28 @@ public class FreeBoardController {
 		service.regist(dto);
 		return "redirect:/freeboard/freeList";
 	}
+	
+	//글 상세보기 처리
+	@GetMapping("/content")
+	public String getContent(int bno, Model model) {
+		model.addAttribute("article", service.getContent(bno));
+		return "freeboard/freeDetail";
+	}
+	
+	//글 수정 요청
+	@PostMapping("/modify")
+	public String modify(FreeModifyRequestDTO dto) {
+		service.update(dto);
+		return "redirect:/freeboard/content?bno=" + dto.getBno();
+	}
+	
+	//글 삭제 요청
+	@PostMapping("/delete")
+	public String delete(int bno) {
+		service.delete(bno);
+		return "redirect:/freeboard/freeList";
+	}
+	
 	
 
 }
--- MyWeb/src/main/java/com/spring/myweb/freeboard/dto/FreeDetailResponseDTO.java
+++ MyWeb/src/main/java/com/spring/myweb/freeboard/dto/FreeDetailResponseDTO.java
@@ -9,32 +9,36 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
-// 클라이언트 측으로 게시글 목록을 줄 때의 스펙 정의.
 @Getter @ToString @EqualsAndHashCode
-public class FreeListResponseDTO {
-
+public class FreeDetailResponseDTO {
+	
 	private int bno;
 	private String title;
 	private String writer;
+	private String content;
 	private String date;
-
-	public FreeListResponseDTO(FreeBoard board) {
-		super();
+	
+	public FreeDetailResponseDTO(FreeBoard board) {
 		this.bno = board.getBno();
 		this.title = board.getTitle();
 		this.writer = board.getWriter();
-		this.date = makePrettierDateString(board.getRegDate());
+		this.content = board.getContent();
+		if(board.getUpdateDate() == null) {
+			this.date = FreeListResponseDTO.makePrettierDateString(board.getRegDate());
+		} else {
+			this.date 
+			= FreeListResponseDTO.makePrettierDateString(board.getUpdateDate()) + " (수정됨)";
+		}
 	}
 
-	static String makePrettierDateString(LocalDateTime regDate) {
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-		return dtf.format(regDate);
-	}
-
-
-
-	
 }
+
+
+
+
+
+
+
 
 
 
